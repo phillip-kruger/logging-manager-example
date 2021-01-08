3bcaf3378a9e862d835466cd5b9adb5dd90f4f4f
Phillip Kruger <phillip.kruger@gmail.com>
2021-01-08 16:24:04 +0200

    Updated the README Signed-off-by:Phillip Kruger <phillip.kruger@gmail.com>

--- src/main/java/com/github/phillipkruger/loggingui/HelloEndpoint.java
+++ src/main/java/com/github/phillipkruger/loggingui/HelloEndpoint.java
@@ -1,8 +1,10 @@
 package com.github.phillipkruger.loggingui;
 
+import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 
 @Path("/hello")
 public class HelloEndpoint {
@@ -10,8 +12,12 @@ public class HelloEndpoint {
     
     @GET
     public String hello(){
-        LOG.info("Someone is saying hello");
-        return "Goeie naand";
+        return hello("Someone");
     }
     
+    @GET @Path("/{name}")
+    public String hello(@PathParam("name") String name){
+        LOG.log(Level.INFO, "{0} is saying hello", name);
+        return "Goeie naand " + name;
+    }
 }
